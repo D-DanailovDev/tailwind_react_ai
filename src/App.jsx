b62be6785e6c79a207058c9900a0f9ecd4d425a3
D.Danailov <ddanailov.dev@gmail.com>
2024-11-11 19:30:04 +0200

    Heading component changed; Services component added; Services component ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Benefits from "./components/Benefits";
-import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
       </div>
 
       <ButtonGradient />
